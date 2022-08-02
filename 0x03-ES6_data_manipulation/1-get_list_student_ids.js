9131e42ffd48cde5ecdbc2c93e27ce164eefb3c7
Eyob14 <eyobzebene3580@gmail.com>
2022-08-02 03:01:59 +0300

    1-get_list_student_ids.js updated

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,7 @@
 export default function getListStudentIds(arr) {
-  if (!Array.isArray(arr)) return [];
+  if (Array.isArray(arr) === false) {
+    return [];
+  }
   const ans = [];
   for (const obj of arr) {
     ans.push(obj.id);
