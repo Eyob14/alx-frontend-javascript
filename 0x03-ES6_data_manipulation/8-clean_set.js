87458643e87161f4ebdadccd0a8f41793beb99e7
Eyob14 <eyobzebene3580@gmail.com>
2022-08-02 18:43:12 +0300

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function cleanSet(set, startString) {
   const array = [...set];
   const ans = [];
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
   array.forEach((value) => {
-    if (typeof startString === 'string' && startString !== '' && value.startsWith(startString)
+    if (value.startsWith(startString)
     ) {
       const res = value.slice(startString.length);
       ans.push(res);
---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const ans = new Map();
+  ans.set('Apples', 10);
+  ans.set('Tomatoes', 10);
+  ans.set('Pasta', 1);
+  ans.set('Rice', 1);
+  ans.set('Banana', 5);
+
+  return ans;
+}
