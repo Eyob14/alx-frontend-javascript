cbe1af3329c53a4b9c470a61358ac96b1b83407d
Eyob14 <eyobzebene3580@gmail.com>
2022-08-02 03:08:36 +0300

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,9 +2,5 @@ export default function getListStudentIds(arr) {
   if (Array.isArray(arr) === false) {
     return [];
   }
-  const ans = [];
-  for (const obj of arr) {
-    ans.push(obj.id);
-  }
-  return ans;
+  return arr.map((student) => student.id);
 }
